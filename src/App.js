a7d03b576357df8b245ee5f68122f47c2482fc31
Mortron <cmsl4y3r@gmail.com>
2021-10-30 14:20:31 -0600

    first react app assignment complete

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li className="listClass">Learn React</li>
+        <li className="listClass">Complete Final MERN Stack</li>
+        <li className="listClass">Complete Projects and Algos</li>
+        <li className="listClass">Keep Learning</li>
+        <li className="listClass"><span>Take over the WORLD!!!!</span></li>
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
